fdeb19a0f64a92eb4dbde059118950ac1df51a48
Cofman <98623505+Cofman@users.noreply.github.com>
2026-05-16 16:09:28 +0300

    add a proper gameplay loop
    You can choose if you want to continue play or not

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ let playerScore = 0;
 let cpuScore = 0;
 let playerChoice = 0;
 let cpuChoice = 0;
+let choiceMain = `Default`;
 
 function funcPlayerChoice() {
-    playerChoice = prompt("1 = Rock, 2 = Paper, 3 = Scissors");
+    playerChoice = prompt("1 = Rock, 2 = Paper, 3 = Scissors", 1);
 }
 
 function funcCPUChoice() {
@@ -27,38 +28,85 @@ function funcGameStart() {
         }
         alert(`Huh, you both chose ${cpuChoiceWord}. So, it's a draw!`);
         alert(`Score: Player: ${playerScore} || CPU: ${cpuScore}`);
+        rockPaperScissorsAgain();
     }
     else if (playerChoice == 1 && cpuChoice == 2) {
         cpuScore += 1;
         alert(`Rock loses to Paper!`);
         alert(`Score: Player: ${playerScore} || CPU: ${cpuScore}`);
+        rockPaperScissorsAgain();
     }
     else if (playerChoice == 1 && cpuChoice == 3) {
         playerScore +=1;
         alert(`Rock beats Scissors!`);
         alert(`Score: Player: ${playerScore} || CPU: ${cpuScore}`);
+        rockPaperScissorsAgain();
     }
     else if (playerChoice == 2 && cpuChoice == 1) {
         playerScore +=1;
         alert(`Paper beats Rock!`)
         alert(`Score: Player: ${playerScore} || CPU: ${cpuScore}`);
+        rockPaperScissorsAgain();
     }
     else if (playerChoice == 2 && cpuChoice == 3) {
         cpuScore +=1;
         alert(`Paper loses to Scissors!`);
         alert(`Score: Player: ${playerScore} || CPU: ${cpuScore}`);
+        rockPaperScissorsAgain();
     }
     else if (playerChoice == 3 && cpuChoice == 1) {
         cpuScore +=1;
         alert(`Scissors loses to Rock!`);
         alert(`Score: Player: ${playerScore} || CPU: ${cpuScore}`);
+        rockPaperScissorsAgain();
     }
     else if (playerChoice == 3 && cpuChoice == 2) {
         playerScore +=1;
         alert(`Scissors beats Paper!`);
         alert(`Score: Player: ${playerScore} || CPU: ${cpuScore}`);
+        rockPaperScissorsAgain();
     }
-    funcGameStart(playerChoice, cpuChoice);
+    else {
+        alert(`Stick to the rules.`);
+        funcGameStart(playerChoice, cpuChoice);
+    }
 }
 
-funcGameStart(playerChoice, cpuChoice);
+function rockPaperScissors() {
+    
+    choiceMain = prompt(`Wanna play Rock, Paper, Scissors? \n Yes or No?`).toLowerCase();
+    if (choiceMain == `yes` || choiceMain == `y`) {
+        funcGameStart(playerChoice, cpuChoice);
+    }
+    else if (choiceMain == `no` || choiceMain == `n`) {
+        alert(`Oh really? See you next time then.`);
+    }
+    else {
+        alert(`That doesn't answer my question...`);
+        rockPaperScissors();
+    }
+}
+
+function rockPaperScissorsAgain() {
+    choiceMain = prompt(`Wanna play again? \n Yes or No?`).toLowerCase();
+    if (choiceMain == `yes` || choiceMain == `y`) {
+        funcGameStart(playerChoice, cpuChoice);
+    }
+    else if (choiceMain == `no` || choiceMain == `n`) {
+        if (playerScore < cpuScore) {
+            alert(`Oh really? See you next time then, loser.`);
+            alert(`Score: Player: ${playerScore} || CPU: ${cpuScore}`);
+        }
+        else {
+            alert(`Oh really? See you next time then.`);
+            alert(`Score: Player: ${playerScore} || CPU: ${cpuScore}`);
+        }
+
+    }
+    else {
+        alert(`That doesn't answer my question...`);
+        rockPaperScissorsAgain();
+    }
+}
+
+rockPaperScissors();
